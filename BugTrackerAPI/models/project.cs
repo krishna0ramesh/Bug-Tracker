25ff800db9188d4002799e134aad5e455bad36e2
Krishna Ramesh <krishnarvpat@gmail.com>
2024-11-18 18:32:18 +0530

    modified project.cs in models(initial commit)

--- BugTrackerAPI/models/project.cs
+++ BugTrackerAPI/models/project.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 public class Project
 {
     public int ProjectId { get; set; }
-    public string ProjectName { get; set; }
-    public string Description { get; set; }
+    public required string ProjectName { get; set; }
+    public required string Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public ICollection<Bug> Bugs { get; set; } // Relationship to Bugs
